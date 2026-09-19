e6176e639e85603eeb6bda878433a460e8318e29
Mohammed Sufiyan <ms9703303@gmail.com>
2026-09-19 16:50:45 +0530

    Restrict CORS to frontend

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ford-price-estimator-1.onrender.com"
+    ],
     allow_methods=["*"],
     allow_headers=["*"]
 )
